ebbc89cc250de8e647905475a934b4a2144a76ab
Seth Silesky <5115498+silesky@users.noreply.github.com>
2022-01-02 13:35:31 -0600

    release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+VERSION="${1:-"patch"}" # major | minor | patch
+
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 if [[ "$BRANCH" != "main" ]]; then
   echo '--> Only execute this on main branch'
@@ -6,16 +8,20 @@ if [[ "$BRANCH" != "main" ]]; then
 fi
 
 # make sure you don't tag an old commit
-git pull --ff-only
+git pull --ff-only &&
 
 # don't create git tag here, it will automatically be created in the next step
-npm version --no-git-tag-version patch -m "Upgrade to %s"
+npm version "$VERSION" -f -m "Upgrade to %s" &&
 
-# github will automatically create a tag when you create a release
+# push after versioning (tags will be pushed in next step)
+# (also relevant: `postVersion:` key: https://docs.npmjs.com/cli/v7/commands/npm-version)
+git push &&
+
+# github will automatically create and push the tag when you create a release
 tag="v$(node -p "require('./package.json').version")"
 gh release create \
   "$tag" \
   --title "$tag" \
-  --notes "releases $tag"
+  --notes "releases $tag" &&
 
 npm publish
